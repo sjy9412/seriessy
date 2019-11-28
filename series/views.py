8ed71921b303ef46a21a48a042426d066b58f2f5
taesu-park <tsoo1014@gmail.com>
2019-11-28 09:36:56 +0900

    aws...

--- series/views.py
+++ series/views.py
@@ -159,32 +159,32 @@ def search(request):
 
 @login_required
 def comment_create_ajax(request):
+    series = get_object_or_404(Series, pk=int(request.POST.get('series_id')))
+    reviews = series.review_set.all()
     if request.method == 'POST':
         for review in reviews:
             if request.user == review.user:
-                forms = ReviewForm(request.POST, instance=review)
-                forms.save()
+                review.content = request.POST.get('content')
+                review.score = int(request.POST.get('score'))
+                review.save()
+                chk = False
                 break
         else:
-            forms = ReviewForm(request.POST)
-            if forms.is_valid():
-                review = forms.save(commit=False)
-                review.user = request.user
-                review.series = series
-                forms.save()
-    return redirect('series:detail', series_pk, movie_pk)
-    review = Review()
-    review.content = request.POST.get('content')
-    review.series = get_object_or_404(Series, pk=int(request.POST.get('series_id')))
-    review.user = request.user
-    review.score = int(request.POST.get('score'))
-    review.save()
-    context = {
-        'content' : review.content,
-        'score' : review.score,
-        'username' : request.user.username
-    }
-    return HttpResponse(json.dumps(context), content_type="application/json")
+            review = Review()
+            review.content = request.POST.get('content')
+            review.series = get_object_or_404(Series, pk=int(request.POST.get('series_id')))
+            review.user = request.user
+            review.score = int(request.POST.get('score'))
+            review.save()
+            chk = True
+        context = {
+            'content' : review.content,
+            'score' : review.score,
+            'username' : request.user.username,
+            'user_id' : request.user.pk,
+            'chk' : chk
+        }
+        return HttpResponse(json.dumps(context), content_type="application/json")
 
 
 def suggest(request):
