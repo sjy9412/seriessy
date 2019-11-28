10d90235aa32e13a341df3ba4641f663e0a50037
sjy9412 <sjy9412@gmail.com>
2019-11-28 18:50:38 +0900

    WS

--- seriessy/asgi.py
+++ seriessy/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seriessy.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seriessy.settings.production")
 django.setup()
 application = get_default_application()
